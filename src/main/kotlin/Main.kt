472f189d007b5ff08624950b3cd72b4e1a2c6624
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2024-01-19 17:28:36 -0500

    Stage 3/6: Look at energy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
+
 package org.example
 
 import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
 
@@ -14,18 +18,59 @@ fun main(args: Array<String>) {
 
     val imageInput = ImageIO.read(File(input))
 
+    val energy = Array(imageInput.width) { Array(imageInput.height) { 0.0 } }
+
     for (x in 0 until imageInput.width) {
         for (y in 0 until imageInput.height) {
-            val color = Color(imageInput.getRGB(x, y))
-            val g = color.green
-            val r = color.red
-            val b = color.blue
+            energy[x][y] = calculateEnergy(x, y, imageInput)
+        }
+    }
 
-            val newColor = Color(255 - r, 255 - g, 255 - b)
-            imageInput.setRGB(x, y, newColor.rgb)
+    val maxEnergy = energy.maxOfOrNull { it.maxOrNull()!! }
+
+    for (w in 0 until imageInput.width) {
+        for (h in 0 until imageInput.height) {
+            val energy = energy[w][h]
+            val intensity = (255.0 * energy / maxEnergy!!).toInt()
+            val alpha = Color(imageInput.getRGB(w, h)).alpha
+            imageInput.setRGB(w, h, Color(intensity, intensity, intensity, alpha).rgb)
         }
     }
 
     val outputFileImage = File(output)
     ImageIO.write(imageInput, "png", outputFileImage)
 }
+
+fun calculateEnergy(x: Int, y: Int, imageInput: BufferedImage): Double {
+    val posix = when (x) {
+        0 -> 1
+        imageInput.width - 1 -> imageInput.width - 2
+        else -> x
+    }
+
+    val posiy = when (y) {
+        0 -> 1
+        imageInput.height - 1 -> imageInput.height - 2
+        else -> y
+    }
+
+    /**
+     * E(0,2)=Δ2x(1,2)+Δ2y(0,2)‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾√
+     */
+
+    val colorX1 = Color(imageInput.getRGB(posix - 1, y))
+    val colorX2 = Color(imageInput.getRGB(posix + 1, y))
+    val colorY1 = Color(imageInput.getRGB(x, posiy - 1))
+    val colorY2 = Color(imageInput.getRGB(x, posiy + 1))
+
+    val deltaX = (colorX1.red - colorX2.red).toDouble().pow(2.0) +
+            (colorX1.green - colorX2.green).toDouble().pow(2.0) +
+            (colorX1.blue - colorX2.blue).toDouble().pow(2.0)
+
+    val deltaY = (colorY1.red - colorY2.red).toDouble().pow(2.0) +
+            (colorY1.green - colorY2.green).toDouble().pow(2.0) +
+            (colorY1.blue - colorY2.blue).toDouble().pow(2.0)
+
+    return sqrt(deltaX + deltaY)
+}
+
